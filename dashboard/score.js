796078c0c4b8764563cc60f1b2ac556e5f8f476b
mph8344 <mph8344@g.rit.edu>
2021-03-02 13:14:48 -0500

    Updated fonts and added reset score

--- dashboard/score.js
+++ dashboard/score.js
@@ -2,16 +2,19 @@
 const team1Rep = nodecg.Replicant('team1score', {defaultValue: 0});
 const team2Rep = nodecg.Replicant('team2score', {defaultValue: 0});
 
+const totalScore = nodecg.Replicant('totalScore', {defaultValue: 0})
 
 function addTeam1() {
 	if (team1Rep.value < 13) {
 		team1Rep.value++;
+		totalScore.value++;
 	}
 }
 
 function subTeam1() {
 	if (team1Rep.value > 0) {
 		team1Rep.value--;
+		totalScore.value--;
 	}
 	
 }
@@ -19,12 +22,20 @@ function subTeam1() {
 function addTeam2() {
 	if (team2Rep.value < 13) {
 		team2Rep.value++;
+		totalScore.value++;
 	}
 }
 
 function subTeam2() {
 	if (team2Rep.value > 0) {
 		team2Rep.value--;
+		totalScore.value--;
 	}
 	
+}
+
+function resetScore() {
+	team1Rep.value = 0;
+	team2Rep.value = 0;
+	totalScore.value = 0;
 }
